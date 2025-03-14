128510c9d058f887b56175421e3774a202dd999d
Krenjeroth <krenjeroth.dev@gmail.com>
2025-03-14 15:12:58 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-Route::group([], function () {
+Route::group(['middleware' => 'auth:sanctum'], function () {
   Route::get('user', function (Request $request) {
     return UserResource::make($request->user());
   });

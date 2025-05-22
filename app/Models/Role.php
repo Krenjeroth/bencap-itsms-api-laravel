99d173dde2f9aea71ec68b0b8ba5bf63a2f07e89
Krenjeroth <blizzmuth@gmail.com>
2025-05-22 11:21:33 +0800

    Update Role.php

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -11,11 +11,11 @@ class Role extends Model
     ];
 
     public function users() {
-    return $this->belongsToMany(User::class);
-  }
+      return $this->belongsToMany(User::class);
+    }
 
-  public function permissions() {
-    return $this->belongsToMany(Permission::class);
-  }
+    public function permissions() {
+      return $this->belongsToMany(Permission::class);
+    }
 
 }

219f732005f34cc48000ce5ee86d45df34dec313
Krenjeroth <blizzmuth@gmail.com>
2025-05-22 14:45:13 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,9 @@ use Illuminate\Http\Request;
 use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\AuthGates;
 
-Route::group(['middleware' => 'auth:sanctum'], function () {
+Route::group(['middleware' => ['auth:sanctum', AuthGates::class]], function () {
   Route::get('user', function (Request $request) {
     return UserResource::make($request->user());
   });

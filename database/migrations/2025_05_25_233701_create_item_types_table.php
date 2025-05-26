47bc42e9090b42575313d7c09f9941dc5cf4957a
Krenjeroth <blizzmuth@gmail.com>
2025-05-26 10:04:43 +0800

    Update 2025_05_25_233701_create_item_types_table.php

--- database/migrations/2025_05_25_233701_create_item_types_table.php
+++ database/migrations/2025_05_25_233701_create_item_types_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('classification')->nullable();
             $table->string('purpose')->nullable();
+            $table->string('type');
             $table->string('status')->nullable();
             $table->timestamps();
         });

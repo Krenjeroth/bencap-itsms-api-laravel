8c3c441768aa78a038cd504a181ff5abea332866
Krenjeroth <blizzmuth@gmail.com>
2025-05-26 11:30:15 +0800

    Update PermissionRoleTableSeeder.php

--- database/seeders/PermissionRoleTableSeeder.php
+++ database/seeders/PermissionRoleTableSeeder.php
@@ -15,11 +15,20 @@ class PermissionRoleTableSeeder extends Seeder
     {
         $admin_permissions = Permission::all();
 
+        $personnel_permissions = Permission::whereIn('title', [
+          'user_index',
+          'user_store',
+          'user_update',
+          'user_show',
+          'user_destroy',
+        ])->get();
+
         $user_permissions = Permission::whereIn('title', [
           'user_index',
         ])->get();
 
-        Role::find(1)->permissions()->attach($admin_permissions); // 1 = Admin
-        Role::find(2)->permissions()->attach($user_permissions); // 2 = User
+        Role::find(1)->permissions()->attach($admin_permissions); // 1 = System Administrator
+        Role::find(2)->permissions()->attach($personnel_permissions); // 2 = Personnel
+        Role::find(3)->permissions()->attach($user_permissions); // 3 = User
     }
 }

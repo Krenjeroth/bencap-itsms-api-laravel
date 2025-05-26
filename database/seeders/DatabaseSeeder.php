6c5a30883d9cc197b950e78a1160f8f4e2a0c9e2
Krenjeroth <blizzmuth@gmail.com>
2025-05-26 11:30:12 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,17 +15,48 @@ class DatabaseSeeder extends Seeder
     {
         User::factory()->create([
           'id' => '88964338-6721-4816-be39-9a9afbe8df95',
-            'name' => 'Admin',
+            'name' => 'System Administrator',
             'email' => 'admin@itsms.com',
         ]);
 
         User::factory()->create([
-          'id' => '152f3ecd-2e69-4cd4-bdc8-43c685602e54',
-            'name' => 'User',
-            'email' => 'user@itsms.com',
+          'id' => 'cf6f9ac2-1cd4-43a8-bfba-acdc431282f9',
+            'name' => 'Krenjer Jan J. Sapitola',
+            'email' => 'krenjerjs@itsms.com',
+        ]);
+
+        User::factory()->create([
+          'id' => '3292e07c-a2a6-40bf-8070-0bb9e76292d6',
+            'name' => 'Brian B. Mang-Oy Jr.',
+            'email' => 'brianbm@itsms.com',
+        ]);
+
+        User::factory()->create([
+          'id' => '5ea07171-e126-4142-9997-9ec24e180e53',
+            'name' => 'Rae Sandy B. Calado',
+            'email' => 'raebc@itsms.com',
+        ]);
+
+        User::factory()->create([
+          'id' => '4885e9d6-7f1c-4283-b1c1-954316312be2',
+            'name' => 'Perseus Pangilinan',
+            'email' => 'perseusp@itsms.com',
+        ]);
+
+        User::factory()->create([
+          'id' => 'e749da8a-8924-44a5-b9a8-b270f43c0e42',
+            'name' => 'Neilsen Kisim',
+            'email' => 'neilsenk@itsms.com',
+        ]);
+
+        User::factory()->create([
+          'id' => 'bf4475b5-257e-4679-b7bc-35941392d659',
+            'name' => 'Lester P. Metua',
+            'email' => 'lesterpm@itsms.com',
         ]);
 
         $this->call([
+          ProfileSeeder::class,
           RoleSeeder::class,
           PermissionSeeder::class,
           PermissionRoleTableSeeder::class,

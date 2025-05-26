3864e9a36785eb142c93bc94a62a6bd371d242f1
Krenjeroth <blizzmuth@gmail.com>
2025-05-26 11:30:18 +0800

    Update ProfileSeeder.php

--- database/seeders/ProfileSeeder.php
+++ database/seeders/ProfileSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Profile;
 use Illuminate\Database\Seeder;
 
 class ProfileSeeder extends Seeder
@@ -12,6 +12,20 @@ class ProfileSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $profiles = [
+          // Administrator
+          ['user_id' => '88964338-6721-4816-be39-9a9afbe8df95', 'display_name' => 'System Admin', 'designation' => 'System Administrator' , 'engagement' => 'ready'],
+          // Personnel
+          ['user_id' => 'cf6f9ac2-1cd4-43a8-bfba-acdc431282f9', 'display_name' => 'KrenjerJS', 'designation' => 'Programmer I' , 'engagement' => 'ready'],
+          ['user_id' => '3292e07c-a2a6-40bf-8070-0bb9e76292d6', 'display_name' => 'BrianBM', 'designation' => 'CMT II' , 'engagement' => 'ready'],
+          ['user_id' => '5ea07171-e126-4142-9997-9ec24e180e53', 'display_name' => 'RaeBC', 'designation' => 'CMT I' , 'engagement' => 'ready'],
+          ['user_id' => '4885e9d6-7f1c-4283-b1c1-954316312be2', 'display_name' => 'PerseusP', 'designation' => 'CMT I' , 'engagement' => 'ready'],
+          ['user_id' => 'e749da8a-8924-44a5-b9a8-b270f43c0e42', 'display_name' => 'NeilsenK', 'designation' => 'CMT I' , 'engagement' => 'ready'],
+          ['user_id' => 'bf4475b5-257e-4679-b7bc-35941392d659', 'display_name' => 'LesterPM', 'designation' => 'CMT I' , 'engagement' => 'ready'],
+          // User
+          // ['user_id' => '152f3ecd-2e69-4cd4-bdc8-43c685602e54', 'display_name' => 'User', 'designation' => 'User' , 'engagement' => 'ready'],
+        ];
+
+        Profile::insert($profiles);
     }
 }

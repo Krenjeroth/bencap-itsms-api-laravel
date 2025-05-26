67e04ba4fe501efcdd7b924d26c6ac746e03d7e0
Krenjeroth <blizzmuth@gmail.com>
2025-05-26 15:20:48 +0800

    Create DepartmentController.php

---
+++ app/Http/Controllers/DepartmentController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Department;
+use App\Http\Resources\DepartmentResource;
+use Illuminate\Support\Facades\Gate;
+
+class DepartmentController extends Controller
+{
+    public function index(Request $request) {
+      // Gate::authorize('department_index');
+
+      $query = Department::query();
+
+      if($request->has('search')) {
+        $search = $request->search;
+        $query->where(function ($q) use($search) {
+          $q->where('name', 'LIKE', "%{$search}%")
+          ->orWhere('full_name', 'LIKE', "%{$search}%")
+          ->orWhere('abbreviation', 'LIKE', "%{$search}%");
+        });
+      }
+
+      // Sorting (default to ID)
+      if ($request->has('sort')) {
+        $order = $request->input('order', 'asc');
+        $query->orderBy($request->sort, $order);
+      }
+
+      // Paginate with customizable per-page count
+      $departments = $query->paginate($request->input('per_page', 5))->appends($request->query());
+
+      return response()->json([
+          'data' => DepartmentResource::collection($departments),
+          'meta' => [
+              'total' => $departments->total(),
+              'per_page' => $departments->perPage(),
+              'current_page' => $departments->currentPage(),
+              'last_page' => $departments->lastPage(),
+          ]
+      ]);
+
+    }
+}

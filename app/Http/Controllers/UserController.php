134573a43f4e15586567a4175224f471ccdff592
Krenjeroth <blizzmuth@gmail.com>
2025-05-27 10:25:50 +0800

    Added Update and Delete Modules for Users Module

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,9 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Gate;
+use App\Http\Requests\StoreUserRequest;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\UpdateUserRequest;
 
 class UserController extends Controller
 {
@@ -46,5 +49,38 @@ class UserController extends Controller
               'last_page' => $users->lastPage(),
           ]
       ]);
+    }
+
+    public function store(StoreUserRequest $request) {
+      Gate::authorize('user_store');
+      
+      $data = $request->validated();
+
+      $user = User::create($data);
+
+      return new UserResource($user);
+    }
+
+    public function update(UpdateUserRequest $request, User $user) {
+      // Gate::authorize('user_update');
+
+      $data = $request->validated();
+
+      $user->update($data);
+
+      return new UserResource($user);
+    }
+
+    public function destroy(User $user) {
+      // Gate::authorize('user_destroy');
+
+      $storage_public = Storage::disk('public');
+      if ($user->img_path && $storage_public->exists($user->img_path)) {
+        $storage_public->delete($user->img_path);
+      }
+
+      $user->delete();
+      
+      return new UserResource($user);
     }
 }

3016e13d44740b5dbf11aa3611cdd4f19286a3bd
Krenjeroth <blizzmuth@gmail.com>
2025-06-10 15:15:42 +0800

    Implement Item Update Module

--- app/Http/Requests/UpdateItemRequest.php
+++ app/Http/Requests/UpdateItemRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateItemRequest extends FormRequest
@@ -11,7 +12,7 @@ class UpdateItemRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return Auth::check();
     }
 
     /**
@@ -22,7 +23,39 @@ class UpdateItemRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'item_type_id' => ['required', 'exists:item_types,id'],
+            'brand_model_id' => ['required', 'exists:brand_models,id'],
+            'parent_component' => ['nullable', 'string', 'max:255'],
+            'code' => ['nullable', 'string', 'max:255', 'unique:items,code,' . $this->item->id],
+            'barcode' => ['nullable', 'string', 'max:255', 'unique:items,barcode,' . $this->item->id],
+            'description' => ['nullable', 'string', 'max:255'],
+            'serial_number' => ['nullable', 'string', 'max:255'],
+            'property_number' => ['required', 'string', 'max:255'],
+            'ics_number' => ['nullable', 'string', 'max:255'],
+            'date_acquired' => ['nullable', 'date'],
+            'ip_address' => ['nullable', 'string', 'max:255'],
+            'mac_address' => ['nullable', 'string', 'max:255'],
+            'inventory_type' => ['nullable', 'string', 'max:255'],
+            'status' => ['nullable', 'string', 'max:255'],
+        ];
+    }
+
+    public function attributes(): array {
+        return [
+            'item_type_id' => 'Item Type',
+            'brand_model_id' => 'Brand Model',
+            'parent_component' => 'Parent Component',
+            'code' => 'Code',
+            'barcode' => 'Barcode',
+            'description' => 'Description',
+            'serial_number' => 'Serial Number',
+            'property_number' => 'Property Number',
+            'ics_number' => 'ICS Number',
+            'date_acquired' => 'Date Acquired',
+            'ip_address' => 'IP Address',
+            'mac_address' => 'MAC Address',
+            'inventory_type' => 'Inventory Type',
+            'status' => 'Status',
         ];
     }
 }

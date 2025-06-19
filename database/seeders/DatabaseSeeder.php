28292270ec79c3a215b1fb36a50ffbdac7a215fd
Krenjeroth <blizzmuth@gmail.com>
2025-06-19 15:25:12 +0800

    Refractor DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,43 +15,49 @@ class DatabaseSeeder extends Seeder
     {
         User::factory()->create([
           'id' => '88964338-6721-4816-be39-9a9afbe8df95',
-            'name' => 'System Administrator',
+            'name' => 'SysAdmin',
             'email' => 'admin@itsms.com',
         ]);
 
         User::factory()->create([
           'id' => 'cf6f9ac2-1cd4-43a8-bfba-acdc431282f9',
-            'name' => 'Krenjer Jan J. Sapitola',
+            // 'name' => 'Krenjer Jan J. Sapitola',
+            'name' => 'KrenjerJS',
             'email' => 'krenjerjs@itsms.com',
         ]);
 
         User::factory()->create([
           'id' => '3292e07c-a2a6-40bf-8070-0bb9e76292d6',
-            'name' => 'Brian B. Mang-Oy Jr.',
+            // 'name' => 'Brian B. Mang-Oy Jr.',
+            'name' => 'BrianBM',
             'email' => 'brianbm@itsms.com',
         ]);
 
         User::factory()->create([
           'id' => '5ea07171-e126-4142-9997-9ec24e180e53',
-            'name' => 'Rae Sandy B. Calado',
+            // 'name' => 'Rae Sandy B. Calado',
+            'name' => 'RaeBC',
             'email' => 'raebc@itsms.com',
         ]);
 
         User::factory()->create([
           'id' => '4885e9d6-7f1c-4283-b1c1-954316312be2',
-            'name' => 'Perseus B. Pangilinan',
-            'email' => 'perseusp@itsms.com',
+            // 'name' => 'Perseus B. Pangilinan',
+            'name' => 'PerseusBP',
+            'email' => 'perseusbp@itsms.com',
         ]);
 
         User::factory()->create([
           'id' => 'e749da8a-8924-44a5-b9a8-b270f43c0e42',
-            'name' => 'Neilsen P. Kisim',
-            'email' => 'neilsenk@itsms.com',
+            // 'name' => 'Neilsen P. Kisim',
+            'name' => 'NeilsenPK',
+            'email' => 'neilsenpk@itsms.com',
         ]);
 
         User::factory()->create([
           'id' => 'bf4475b5-257e-4679-b7bc-35941392d659',
-            'name' => 'Lester P. Metua',
+            // 'name' => 'Lester P. Metua',
+            'name' => 'LesterPM',
             'email' => 'lesterpm@itsms.com',
         ]);
 
--- database/seeders/ProfileSeeder.php
+++ database/seeders/ProfileSeeder.php
@@ -14,14 +14,14 @@ class ProfileSeeder extends Seeder
     {
         $profiles = [
           // Administrator
-          ['user_id' => '88964338-6721-4816-be39-9a9afbe8df95', 'display_name' => 'System Admin', 'designation' => 'System Administrator' , 'engagement' => 'ready'],
+          ['user_id' => '88964338-6721-4816-be39-9a9afbe8df95', 'display_name' => 'System Administrator', 'designation' => 'System Administrator' , 'engagement' => 'ready'],
           // Personnel
-          ['user_id' => 'cf6f9ac2-1cd4-43a8-bfba-acdc431282f9', 'display_name' => 'KrenjerJS', 'designation' => 'Programmer I' , 'engagement' => 'ready'],
-          ['user_id' => '3292e07c-a2a6-40bf-8070-0bb9e76292d6', 'display_name' => 'BrianBM', 'designation' => 'CMT II' , 'engagement' => 'ready'],
-          ['user_id' => '5ea07171-e126-4142-9997-9ec24e180e53', 'display_name' => 'RaeBC', 'designation' => 'CMT I' , 'engagement' => 'ready'],
-          ['user_id' => '4885e9d6-7f1c-4283-b1c1-954316312be2', 'display_name' => 'PerseusBP', 'designation' => 'CMT I' , 'engagement' => 'ready'],
-          ['user_id' => 'e749da8a-8924-44a5-b9a8-b270f43c0e42', 'display_name' => 'NeilsenPK', 'designation' => 'CMT I' , 'engagement' => 'ready'],
-          ['user_id' => 'bf4475b5-257e-4679-b7bc-35941392d659', 'display_name' => 'LesterPM', 'designation' => 'CMT I' , 'engagement' => 'ready'],
+          ['user_id' => 'cf6f9ac2-1cd4-43a8-bfba-acdc431282f9', 'display_name' => 'Krenjer Jan J. Sapitola', 'designation' => 'Computer Programmer I' , 'engagement' => 'ready'],
+          ['user_id' => '3292e07c-a2a6-40bf-8070-0bb9e76292d6', 'display_name' => 'Brian B. Mang-Oy Jr.', 'designation' => 'CMT II' , 'engagement' => 'ready'],
+          ['user_id' => '5ea07171-e126-4142-9997-9ec24e180e53', 'display_name' => 'Rae Sandy B. Calado', 'designation' => 'CMT I' , 'engagement' => 'ready'],
+          ['user_id' => '4885e9d6-7f1c-4283-b1c1-954316312be2', 'display_name' => 'Perseus B. Pangilinan', 'designation' => 'CMT I' , 'engagement' => 'ready'],
+          ['user_id' => 'e749da8a-8924-44a5-b9a8-b270f43c0e42', 'display_name' => 'Neilsen P. Kisim', 'designation' => 'CMT I' , 'engagement' => 'ready'],
+          ['user_id' => 'bf4475b5-257e-4679-b7bc-35941392d659', 'display_name' => 'Lester P. Metua', 'designation' => 'CMT I' , 'engagement' => 'ready'],
           // User
           // ['user_id' => '152f3ecd-2e69-4cd4-bdc8-43c685602e54', 'display_name' => 'User', 'designation' => 'User' , 'engagement' => 'ready'],
         ];

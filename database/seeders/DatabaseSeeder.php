74ce5fc35605790391b94fe6facc77f32fa06246
Krenjeroth <blizzmuth@gmail.com>
2025-07-17 16:04:35 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -67,6 +66,7 @@ class DatabaseSeeder extends Seeder
           PermissionSeeder::class,
           PermissionRoleTableSeeder::class,
           RoleUserTableSeeder::class,
+          MeasurementUnitSeeder::class,
         ]);
     }
 }

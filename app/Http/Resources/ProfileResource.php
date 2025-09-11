3eae3843804ab5af7849c83758802275ca74fe48
Krenjeroth <blizzmuth@gmail.com>
2025-09-11 11:20:07 +0800

    Update ProfileResource.php
    Display humanized date diff from personnel last seen when offline.

--- app/Http/Resources/ProfileResource.php
+++ app/Http/Resources/ProfileResource.php
@@ -3,7 +3,9 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Carbon\CarbonInterface;
 
 class ProfileResource extends JsonResource
 {
@@ -23,7 +25,9 @@ class ProfileResource extends JsonResource
           'designation' => $this->designation,
           'status' => $this->status, // online, offline
           'status_text' => $this->status_text,
-          'engagement' => $this->engagement, // ready, 
+          'engagement' => $this->engagement, // ready, busy
+          'last_seen_at' => $this->last_seen_at,
+          'last_seen_at_humanized' => Carbon::parse($this->last_seen_at)->diffForHumans(["short" => true, 'syntax' => CarbonInterface::DIFF_ABSOLUTE, "options" => Carbon::NO_ZERO_DIFF]),
           'img_path' => $this->img_path ? asset("storage/{$this->img_path}") : null,
           'created_at' => $this->created_at,
           'updated_at' => $this->updated_at,

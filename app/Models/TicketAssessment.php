3da20b17d0b2ff31931c927b12ed8b6d4083f740
Krenjeroth <blizzmuth@gmail.com>
2026-05-28 15:06:46 +0800

    Update TicketAssessment.php

--- app/Models/TicketAssessment.php
+++ app/Models/TicketAssessment.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class TicketAssessment extends Model
 {
-    //
+    protected $fillable = [
+        'ticket_id',
+        'findings',
+        'recommendations',
+        'replacement_available',
+        'specifications',
+        'components',
+        'reviewed_by',
+        'assessed_by',
+        'reviewed_by_position',
+        'assessed_by_position',
+    ];
+
+    protected $casts = [
+        'components'             => 'array',
+        'replacement_available'  => 'boolean',
+    ];
+
+    public function ticket() {
+        return $this->belongsTo(Ticket::class);
+    }
 }
